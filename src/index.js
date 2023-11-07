941548221b22ef9a7cab11a2b25187b05f5302bd
Vladimir Kovalenko <dante77758@gmail.com>
2023-11-07 22:15:46 +0200

    add files

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+import { cars } from './js/cars-items';
+
+const conteiner = document.querySelector('.js-conteiner');
+
+const markup = cars
+  .map(
+    ({ img, car, id }) => `
+       <li data-car-id="${id}" class="js-target js-card">
+        <img src="${img}" alt="${car}" class="js-target" />
+        <h2 class="js-target">${car}</h2>
+      </li>`
+  )
+  .join('');
+
+conteiner.insertAdjacentHTML('beforeend', markup);
+
+conteiner.addEventListener('click', onClick);
+
+function onClick(e) {
+  const { target } = e;
+  if (!target.classList.contains('js-target')) {
+    return;
+  }
+  const carId =
+    target.dataset.carId ?? target.closest('.js-card').dataset.carId;
+  const currentItem = cars.find(({ id }) => id === Number(carId));
+
+  // Якщо не знайло id напріклад не прийшов з бекенду
+  if (!currentItem) {
+    const instance = basicLightbox.create(
+      `
+    <div class = "wrapper">
+    <img src="https://t4.ftcdn.net/jpg/05/07/58/41/360_F_507584110_KNIfe7d3hUAEpraq10J7MCPmtny8EH7A.jpg" />
+    </div>
+`
+    );
+    instance.show();
+    return;
+  } else {
+    const instance = basicLightbox.create(`
+    <div class = "wrapper">
+      <img src="${currentItem.img}" alt="${currentItem.car}" />
+      <h2>${currentItem.car}</h2>
+      <h3>${currentItem.type}</h3>
+      <p>Ціна: ${currentItem.price} грн.</p>
+    </div>
+`);
+    instance.show();
+  }
+}
---
+++ src/js/cars-items.js
@@ -0,0 +1,41 @@
+const cars = [
+  {
+    id: 1,
+    car: 'Honda',
+    type: 'Civic',
+    price: 12000,
+    img: 'https://cars.ua/thumb/car/20180914/w933/h622/q80/kupit-honda-civic-4d-lvov-1368471.jpeg',
+  },
+
+  {
+    id: 2,
+    car: 'Honda',
+    type: 'Accord',
+    price: 16000,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/2018_Honda_Accord_12.17.17.jpg/1200px-2018_Honda_Accord_12.17.17.jpg',
+  },
+  {
+    id: 3,
+    car: 'Toyota',
+    type: 'Corolla',
+    price: 11000,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Toyota_Corolla_Limousine_Hybrid_Genf_2019_1Y7A5576.jpg',
+  },
+
+  {
+    id: 4,
+    car: 'Toyota',
+    type: 'Camry',
+    price: 17000,
+    img: 'https://kievinform.com/wp-content/uploads/2020/11/toyota_camry.jpg',
+  },
+
+  {
+    car: 'Tesla',
+    type: 'Model 3',
+    price: 25000,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/8/83/Tesla_Model_3_parked%2C_front_driver_side.jpg',
+  },
+];
+
+export { cars };
